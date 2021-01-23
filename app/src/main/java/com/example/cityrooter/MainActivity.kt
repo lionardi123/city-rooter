f67f676533fdcfb33d52d9e70b5f79893403eaa5
Yulius Lionardi <yulius.lionardi@traveloka.com>
2021-01-23 11:16:07 +0700

    Implement navigationItem

--- app/src/main/java/com/example/cityrooter/MainActivity.kt
+++ app/src/main/java/com/example/cityrooter/MainActivity.kt
@@ -5,15 +5,27 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.ActionBar
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cityrooter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setDisplayShowCustomEnabled(true);
         supportActionBar?.setCustomView(R.layout.action_bar_layout)
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.blue_primary));
+    }
 
-        setContentView(R.layout.activity_main)
+    override fun onStart() {
+        super.onStart()
+        val navController = findNavController(R.id.navHostContainer)
+        activityMainBinding.bottomNavbar.setupWithNavController(navController)
     }
 }
