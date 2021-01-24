d5ee42d6d8688624e1edfd371ca4e0c3968d976d
Yulius Lionardi <yulius.lionardi@traveloka.com>
2021-01-24 12:51:25 +0700

    Update Json

--- app/src/main/java/com/example/cityrooter/datamodel/HomeResponseModel.kt
+++ app/src/main/java/com/example/cityrooter/datamodel/HomeResponseModel.kt
@@ -4,11 +4,17 @@ data class HomeResponseModel(
     val headerImageList: List<String>,
     val bannerImage: String,
     val mainInfoList: List<TitleLabelImageModel>,
+    val sellingPointsBanner: String,
     val sellingPoints: List<TitleLabelImageModel>,
     val adVideo: String,
+    val aboutUsBanner: String,
     val aboutUs: String,
+    val custSummariesBanner: String,
     val custSummaries: List<CustSummaryModel>,
+    val superioritiesBanner: String,
     val superiorities: List<String>,
+    val servicesBanner: String,
     val services: List<String>,
+    val galleriesBanner: String,
     val galleries: List<String>
 )
